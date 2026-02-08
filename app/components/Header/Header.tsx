e0c2aa1ddbcdf14b2f73172cda3e8f464d21a0d9
HPCopc <prhpco@gmail.com>
2026-02-07 20:55:36 -0500

    Update and rename page.tsx to Header.tsx

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="container">
+        <h1 className="brand">Header</h1>
+        <nav className="nav">
+          /Home</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+

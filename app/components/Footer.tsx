d4ea2c886d1dbd7b081c686a4e374d3ba7696414
HPCopc <prhpco@gmail.com>
2026-02-16 13:04:46 -0500

    init16

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,21 @@
 // 'use client';   ✅ Add this if you want to be safe
 
  import { View, Flex, Text } from "@aws-amplify/ui-react";
+ 
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <div className="flex flex-row
-     items-center justify-center p-4 bg-gray-100">
-      <div className="mb-2">
+    <div className="flex flex-col">
+      {/* First container - Gray background with Topics */}
+      <div className="bg-gray-100 p-4 text-center">
         <p className="text-gray-600">Topics more services</p>
       </div>
 
-      <div className="mt-2">
-        <p className="text-sm text-gray-500">© 2024 Your Company</p>
+      {/* Second container - Black background with Copyright */}
+      <div className="bg-black text-white p-4 text-center ">
+        <p className="text-sm text-white">© {currentYear} Your Company</p>
       </div>
     </div>
   );

37900b2c4678a6a201b371e8c6685bc71b3fef2f
HPCopc <prhpco@gmail.com>
2026-02-21 12:53:50 -0500

    frontend2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@
 
  import { View, Flex, Text } from "@aws-amplify/ui-react";
  import Link from 'next/link';
- import NavItem from '../types/navigation';
+ import { NavItem } from '../types/navigation';
  
    const footerleft: NavItem[] = [
     { label: 'Home', href: '/about' },

a1731fa030994b122f8b277a1a23178d398feebb
HPCopc <prhpco@gmail.com>
2026-03-08 19:31:21 -0400

    fix: deploy empty backend to reset CloudFormation stacks

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,4 @@
 import { defineBackend } from '@aws-amplify/backend';
-// import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { postConfirmation } from '../app/functions/Post-confirmation/resource';  
+  
 
-defineBackend({
- // auth,
-  data,
-  postConfirmation, 
-});
+defineBackend({ });

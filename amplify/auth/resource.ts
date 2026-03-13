241209c91b807edfcbd9a8b0250489b010977c2f
HPCopc <prhpco@gmail.com>
2026-03-13 18:30:23 -0400

    login 13a

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -19,8 +19,8 @@ export const auth = defineAuth({
 
   // User attributes stored in Cognito
   userAttributes: {
-    givenName: { required: yes, mutable: true },
-    familyName: { required: yes, mutable: true },
+    givenName: { required: true, mutable: true },
+    familyName: { required: true, mutable: true },
     phoneNumber: { required: false, mutable: true },
   },
   // Add the post-confirmation trigger

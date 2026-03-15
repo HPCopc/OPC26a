3a05b268c55c7bb069418cc89a8e2e6895966968
HPCopc <prhpco@gmail.com>
2026-03-15 17:54:17 -0400

    fix: use Authenticator.SignUp.FormFields, set required false for name fields

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -19,8 +19,8 @@ export const auth = defineAuth({
 
   // User attributes stored in Cognito
   userAttributes: {
-    givenName: { required: true, mutable: true },
-    familyName: { required: true, mutable: true },
+    givenName: { required: false, mutable: true },
+    familyName: { required: false, mutable: true },
     phoneNumber: { required: false, mutable: true },
   },
   // Add the post-confirmation trigger

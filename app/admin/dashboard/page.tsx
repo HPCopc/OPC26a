9fdc81f15c0ac8fa975ac5628b8cce953aa76884
HPCopc <prhpco@gmail.com>
2026-05-28 16:06:11 -0400

    fixed amplify confi outputs

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -3,9 +3,9 @@
 import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '@/amplify/data/resource';
-import { Amplify } from 'aws-amplify'; 
-import outputs from '@/amplify_outputs.json';
-Amplify.configure(outputs);
+// import { Amplify } from 'aws-amplify'; 
+// import outputs from '@/amplify_outputs.json';
+// Amplify.configure(outputs);
 
 const client = generateClient<Schema>({ authMode: 'userPool' });
 

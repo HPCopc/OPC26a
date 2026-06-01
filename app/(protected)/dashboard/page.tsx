462563d8a9f8bedb8744c239177bee9ec8dd8506
HPCopc <prhpco@gmail.com>
2026-06-01 16:14:59 -0400

    login goes to home page from dashboard

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function DashboardPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/'); // Always go home
+  }, [router]);
+
+  return null;
+}
